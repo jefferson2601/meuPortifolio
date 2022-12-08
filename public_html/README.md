65c613d82abe9b8055c4a69ce28fe28be68fd7fa
jefferson2601 <jefferson260114@gmail.com>
2022-12-08 16:58:52 -0300

    adicionando meu projeto ao repositorio no github

---
+++ public_html/README.md
@@ -0,0 +1,1 @@
+criando meu portifólio
---
+++ public_html/contato.php
@@ -0,0 +1,12 @@
+<?php
+
+$vnome = $_POST["nome"];
+$vsobrenome = $_POST["sobrenome"];
+$vemail = $_POST["email"];
+$vmensagem = $_POST["mensagem"];
+
+
+
+echo "Nome: ".$vnome."<br/>Sobrenome: ".$vsobrenome."<br/>Email: ".$vemail."<br/>Mensagem: ".$vmensagem."<br/>";
+
+?>
